a85919cff1249d30f97bcf79701f71cc7f74055d
JP Brunache <kosmik7@users.noreply.github.com>
2023-06-27 18:02:07 +0200

    Améliorations de NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./NavBar.module.scss";
 import data from "../assets/data-navlinks.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function NavLinks() {
-  return (
-    <>
-      <ul>
-        {data.map((item) => (
-          <li key={item.name}>
-            <a href={item.link}>{item.name}</a>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
-
-function Hamburger() {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
-  const menuisOpen = isOpen ? styles.isOpen : "";
-
-  return (
-    <>
-      <div
-        className={styles.hamburger + " " + menuisOpen}
-        onClick={handleClick}
-      >
-        <FontAwesomeIcon icon="fa-solid fa-bars" />
-      </div>
-    </>
-  );
-}
-
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuisOpen = isOpen ? styles.isOpen : "";
+  const ref = useRef();
+
+  // Ferme le menu si click en dehors (de ref)
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (isOpen && ref.current && !ref.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [isOpen]);
+
   return (
-    <header className={styles.header}>
-      <Hamburger />
-      <NavLinks />
+    <header className={styles.header} ref={ref}>
+      <div className={styles.navContainer}>
+        <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+          <FontAwesomeIcon icon="fa-solid fa-bars" size="xl" />
+        </div>
+        <ul className={menuisOpen}>
+          {data.map((item) => (
+            <li key={item.name}>
+              <a href={item.link} onClick={() => setIsOpen(false)}>
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 }
